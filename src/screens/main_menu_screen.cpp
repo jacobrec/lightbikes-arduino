333be1c50cef875aa3e723b68ec565392c00e75f
Jacob Reckhard <jacobreckhard@gmail.com>
2017-11-25 15:39:06 -0700

    whoops, shouldn't have commited this

--- src/screens/main_menu_screen.cpp
+++ src/screens/main_menu_screen.cpp
@@ -1,10 +1,8 @@
 // IMPORTANT: this file is only to be included into screenManager.cpp and nowhere else
-MainMenuScreen::MainMenuScreen() {// this is the constructor
+MainMenuScreen::MainMenuScreen() { // this is the constructor
 }
-int i = 0;
+
 void MainMenuScreen::frame() { // this runs every frame
-    Serial.print("Changing screens: ");
-    Serial.println(i++);
     this->changeScreen(new GameScreen(new StayingAlive_Driver(), new StayingAlive_Driver()));
 }
 
