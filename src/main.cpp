18d039d063990cb6740e3053268e0582d9f595ca
Jacob Reckhard <jacobreckhard@gmail.com>
2017-11-24 22:23:22 -0700

    changed simple delay to a system to prevent slowdowns stacking
    this works by only delaying an amount need to assure a constant framerate, of course this will fail when the length of time to render and update exceeds the millisPerCycle value

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,34 @@
 #include "game.h"
 #include "drawing.h"
 
-
+#define millisPerCycle    50 /* 50 is milliseconds */
+void ensureFrameRate(long frameCount);
 
 int main() {
     init();
     Serial.begin(9600);
-    Serial.println("Starting program");
 
     //setup game
-    Grid_t* grid = initWorld(64, 48);
+    Grid_t *grid       = initWorld(64, 48);
+    long    frameCount = 0;
 
     // main game loop
-    while(true){
-        Serial.println("updating starting");
+    while (true) {
         update(grid); // updates game world
-        Serial.println("rendering starting");
         render(grid); // renders the world
-        Serial.println("waiting for delay");
-        delay(50);// 50 is milliseconds
-
+        ensureFrameRate(frameCount++);
     }
 
     Serial.flush();
     Serial.end();
 
-    return 0;
+    return(0);
+}
+
+void ensureFrameRate(long frameCount) {
+    long diff = frameCount * millisPerCycle - millis();
+
+    if (diff > 0) {
+        delay(diff);
+    }
 }
