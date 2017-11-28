73938daf399b19cd0f54ea36782f21310952c5e9
Jacob Reckhard <jacobreckhard@gmail.com>
2017-11-28 13:17:28 -0700

    made note of max size of grid

--- src/screens/game_screen.cpp
+++ src/screens/game_screen.cpp
@@ -1,6 +1,6 @@
 // IMPORTANT: this file is only to be included into screenManager.cpp and nowhere else
 GameScreen::GameScreen(Driver_t *d1, Driver_t *d2) {
-    this->grid = initWorld(64, 48, d1, d2);  // create a new grid, the central game world object, of size 64x48
+    this->grid = initWorld(64, 48, d1, d2);  // create a new grid, the central game world object, of size 64x48, max size is 160x120
     drawGrid(this->grid);                    // initializes the graphics system, as well as draws boarder
 }
 
