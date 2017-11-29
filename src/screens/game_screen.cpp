65fccd0050f9b2f98c0934b9f9e841e8ce94f9cc
Jacob Reckhard <jacobreckhard@gmail.com>
2017-11-29 14:28:59 -0700

    sets text size in gameScreen

--- src/screens/game_screen.cpp
+++ src/screens/game_screen.cpp
@@ -2,6 +2,7 @@
 GameScreen::GameScreen(Driver_t *d1, Driver_t *d2) {
     //this->grid = initWorld(64, 48, d1, d2);  // create a new grid, the central game world object, of size 64x48
     //this->grid = initWorld(160, 120, d1, d2);
+    tft.setTextSize(5);
     this->grid = initWorld(32, 24, d1, d2);
     drawGrid(this->grid);                    // initializes the graphics system, as well as draws boarder
     render(this->grid);               // renders the world
